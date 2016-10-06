1b45d25e1564e1a823280e6ce487a72147d55b54
Joanthan Greve <jmgre16@student.monash.edu>
2016-10-06 21:47:44 +0200

    Add a new project

--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -1,10 +1,18 @@
 'use strict';
 
 angular.module('tangentApp')
-  .controller('ProjectsCtrl', function ($scope, $http, $cookies, $location, tokenFetch) {
+  .controller('ProjectsCtrl', function ($scope, $http, $cookies, $location, tokenFetch, $filter) {
 
     $scope.token;
     $scope.projects = [];
+    $scope.addData = {
+      title: "",
+      description: "",
+      start_date: null,
+      end_date: null,
+      is_billable: null,
+      is_active: null
+    };
 
     /* initial scope loading function, checks if the token is still valid in the cookie, and not yet expired */
     $scope.init = function(){
@@ -36,6 +44,30 @@ angular.module('tangentApp')
 
         });
       }
+    }
+
+    $scope.clickAdd = function(){
+      $('#ProjectAddModal').modal('show');
+    }
+    $scope.addProject = function(data){
+
+
+      data.start_date =$filter('date')(new Date(data.start_date), "yyyy-MM-dd");
+      data.end_date =$filter('date')(new Date(data.end_date), "yyyy-MM-dd");
+      console.log(data);
+      $http.post('http://projectservice.staging.tangentmicroservices.com:80/api/v1/projects/',data, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': $scope.token
+        }}).
+      success(function (data, status, headers, config) {
+        console.log(data);
+        $scope.projects.push(data);
+      }).error(function (error, status) {
+        console.log(error);
+
+
+      });
     }
 
     $scope.init();
