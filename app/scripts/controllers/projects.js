ed707654b904b2110fc49666066e54e415dd5b0c
Joanthan Greve <jmgre16@student.monash.edu>
2016-10-07 10:44:58 +0200

    Project Delete and neatened up UI a bit

--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -40,21 +40,39 @@ angular.module('tangentApp')
               'Authorization': $scope.token
             }
           }).success(function(data, status, headers, config) {
+          angular.forEach(data, function(d){
+            if(d.task_set.length >0){
+              angular.forEach(d.task_set, function(ts){
+                if(ts.due_date != null){
+                  ts.due_date = new Date(ts.due_date);
+                }
+                else{
+                  ts.due_date = undefined;
+                }
+
+              });
+            }
+          });
           console.log(data);
           $scope.projects  = data;
-
         });
       }
     }
+
     /* add project function */
+
     $scope.clickAdd = function(){
       $('#ProjectAddModal').modal('show');
     }
     $scope.addProject = function(data){
-
-
       data.start_date =$filter('date')(new Date(data.start_date), "yyyy-MM-dd");
       data.end_date =$filter('date')(new Date(data.end_date), "yyyy-MM-dd");
+      if(data.is_billable == null){
+        data.is_billable = false;
+      }
+      if(data.is_active == null){
+        data.is_active = false;
+      }
       console.log(data);
       $http.post('http://projectservice.staging.tangentmicroservices.com:80/api/v1/projects/',data, {
         headers: {
@@ -64,6 +82,7 @@ angular.module('tangentApp')
       success(function (data, status, headers, config) {
         console.log(data);
         $scope.projects.push(data);
+        $('#ProjectAddModal').modal('hide');
       }).error(function (error, status) {
         console.log(error);
       });
@@ -73,6 +92,32 @@ angular.module('tangentApp')
       $scope.selProject = project;
       $('#ProjectViewModal').modal('show');
     }
+
+    /* Project Delete */
+
+    $scope.clickDelete = function(p){
+      $scope.selProject = p;
+      $('#ProjectDeleteModal').modal('show');
+    }
+
+    $scope.delProject= function(){
+      var ind = $scope.projects.indexOf($scope.selProject);
+
+      $http.delete('http://projectservice.staging.tangentmicroservices.com:80/api/v1/projects/'+$scope.selProject.pk+'/', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': $scope.token
+        }}).
+      success(function (data, status, headers, config) {
+        console.log(data);
+        $scope.projects.splice(ind, 1);
+        $scope.selProject = null;
+        $('#ProjectDeleteModal').modal('hide');
+      }).error(function (error, status) {
+        console.log(error);
+      });
+    }
+
 
     $scope.init();
   });
